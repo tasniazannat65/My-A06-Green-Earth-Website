4bfe05b03d93999ae52e3149e3bccd6d24c92da8
tasniazannat65 <tasniazannat9@gmail.com>
2025-09-09 04:30:33 +0600

    Added  modal functionality for plant details and active category button highlight

--- script/index.js
+++ script/index.js
@@ -6,10 +6,16 @@ const loadCategories = () => {
     .then((res) => res.json())
     .then((data) => {
         showCategories(data.categories);
+
+        if(data.categories.length > 0){
+        const plantCategoryId = data.categories[0].id;
+        loadTreesCategories(plantCategoryId);
+    }
     })
     .catch(err => {
         console.log(err);
     })
+    
 };
 // the functionality of load categories is showing in the display
 const showCategories = (categories) => {
@@ -22,13 +28,29 @@ const showCategories = (categories) => {
     const categoriesBtn = document.createElement('div');
     categoriesBtn.innerHTML = `
      <div>
-     <p onclick="loadTreesCategories('${category.id}')" class="p-3 rounded-md text-[#1F2937] bg-[#F0FDF4] w-full text-left cursor-pointer  hover:bg-green-500 hover:text-white">${category.category_name}</p>
+     <p id="${category.id}" onclick="loadTreesCategories('${category.id}')" class="category-btn p-3 rounded-md text-[#1F2937] bg-[#F0FDF4] w-full text-left cursor-pointer  hover:bg-green-500 hover:text-white">${category.category_name}</p>
      </div>
 
     `
     categoriesContainer.append(categoriesBtn);
 
    });
+     
+   categoriesContainer.addEventListener('click', (event) => {
+    if(event.target.className.includes('category-btn')){
+        const allCategories = document.querySelectorAll('.category-btn');
+        allCategories.forEach(p => {
+            p.classList.remove('bg-[#15803D]', 'text-white');
+            p.classList.add('bg-[#F0FDF4]');
+        })
+        event.target.classList.add('bg-[#15803D]', 'text-white');
+        event.target.classList.remove('bg-[#F0FDF4]');
+        loadTreesCategories(event.target.id);
+    }
+   })
+
+
+
 
 
 };
@@ -40,11 +62,13 @@ const loadTreesCategories = (plantId) => {
     .then((res) => res.json())
     .then((data) => {
         showTreesCategories(data.plants);
+      
     
     })
     .catch(err => {
         console.log(err);
     })
+
     
 };
 
@@ -61,7 +85,7 @@ const showTreesCategories = (plants) => {
        
          <div class="bg-white p-4 rounded-lg space-y-4 shadow-sm">
          <img src="${plant.image}" alt="${plant.name}" class="rounded-lg w-full h-44 object-cover">
-         <h2 class="font-semibold text-xl">${plant.name}</h2>
+         <h2 onclick="loadPlantDetails('${plant.id}')" class=" font-semibold text-xl cursor-pointer">${plant.name}</h2>
          <p class="text-[#1F2937] text-[12px]">${plant.description}</p>
          <div class="flex justify-between items-center">
          <div class="badge font-geist font-medium text-[#15803D] bg-[#DCFCE7] flex justify-center items-center rounded-full  px-6 py-2 min-w-[50px] h-10">${plant.category}</div>
@@ -74,11 +98,43 @@ const showTreesCategories = (plants) => {
         
         
         `
-        plantsContainer.append(plantCard)
+        plantsContainer.append(plantCard);
     })
 
 
 
 };
+// the functionality of load plant details
+const loadPlantDetails = (id) => {
+    const url = `https://openapi.programming-hero.com/api/plant/${id}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        
+        showPlantDetails(data.plants);
+    })
+    .catch(err => {
+        console.log(err);
+    })
+};
+// the functionality of showing full tree details to open a modal by clicking the plant name.
+const showPlantDetails = (details) => {
+    
+    const modalDetails = document.getElementById('plant-details-container');
+    modalDetails.innerHTML = `
+     <div class="p-2 space-y-3">
+                <h2 class="text-xl font-bold">${details.name}</h2>
+                <img src="${details.image}" alt="${details.name}" class="rounded-lg w-full h-56 object-cover">
+                <p><span  class="font-bold text-lg">Category:</span> ${details.category}</p>
+                <p><span class="font-bold text-lg">Price:</span> ৳${details.price}</p>
+                <p><span class="font-bold text-lg">Description:</span> ${details.description}</p>
+            </div>
+    
+    `
+    document.getElementById('details_modal').showModal();
+
+}
+
 
 loadCategories();
+
