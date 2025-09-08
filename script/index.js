6976b932d18ff86d88968ed8770e3aec03bd5ad9
tasniazannat65 <tasniazannat9@gmail.com>
2025-09-09 00:48:29 +0600

    Display plant cards on category selection

--- script/index.js
+++ script/index.js
@@ -22,7 +22,7 @@ const showCategories = (categories) => {
     const categoriesBtn = document.createElement('div');
     categoriesBtn.innerHTML = `
      <div>
-     <button class="p-3 rounded-md text-[#1F2937] bg-[#F0FDF4] w-full text-left  hover:bg-green-500 hover:text-white shadow-sm ">${category.category_name}</button>
+     <p onclick="loadTreesCategories('${category.id}')" class="p-3 rounded-md text-[#1F2937] bg-[#F0FDF4] w-full text-left cursor-pointer  hover:bg-green-500 hover:text-white">${category.category_name}</p>
      </div>
 
     `
@@ -31,8 +31,54 @@ const showCategories = (categories) => {
    });
 
 
-}
+};
+
+// the functionality of load tree categories
+const loadTreesCategories = (plantId) => {
+    const url = `https://openapi.programming-hero.com/api/category/${plantId}`;
+    fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+        showTreesCategories(data.plants);
+    
+    })
+    .catch(err => {
+        console.log(err);
+    })
+    
+};
 
 
+// the functionality of showing the  plant card on display  by clicking a category
+const showTreesCategories = (plants) => {
+    const plantsContainer = document.getElementById('plants-container');
+    plantsContainer.innerHTML = '';
+    
+    plants.forEach(plant => {
+      
+        const plantCard = document.createElement('div');
+        plantCard.innerHTML = `
+       
+         <div class="bg-white p-4 rounded-lg space-y-4 shadow-sm">
+         <img src="${plant.image}" alt="${plant.name}" class="rounded-lg w-full h-44 object-cover">
+         <h2 class="font-semibold text-xl">${plant.name}</h2>
+         <p class="text-[#1F2937] text-[12px]">${plant.description}</p>
+         <div class="flex justify-between items-center">
+         <div class="badge font-geist font-medium text-[#15803D] bg-[#DCFCE7] flex justify-center items-center rounded-full  px-6 py-2 min-w-[50px] h-10">${plant.category}</div>
+         <p class="font-semibold text-lg">৳${plant.price}</p>
+         </div>
+         <button class="btn bg-[#15803D] text-white text-lg rounded-full w-full hover:bg-green-400">Add to Cart</button>
+         </div>
+        
+        
+        
+        
+        `
+        plantsContainer.append(plantCard)
+    })
+
+
+
+};
 
 loadCategories();
